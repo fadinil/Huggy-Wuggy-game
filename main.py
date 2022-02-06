0a52e8e6c6592b9cb1bc204b651bdeafd6edbded
Ilya F <91550079+fadinil@users.noreply.github.com>
2022-02-06 03:09:18 +0300

    Add road

--- main.py
+++ main.py
@@ -4,18 +4,21 @@ pygame.init()
 W = 600
 H = 400
 
-sc = pygame.display.set_mode((W, H), pygame.RESIZABLE)
+sc = pygame.display.set_mode((W, H))
 pygame.display.set_caption("Hagi-wagi")
 
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
 
 FPS = 60
 clock = pygame.time.Clock()
-
-x = W // 2
+road_chunks = [
+    [pygame.image.load('sprites/road.png'), [0, H - 100]],
+    [pygame.image.load('sprites/road.png'), [2404, H - 100]]
+]
 y = H // 2
 speed = 5
 
@@ -23,19 +26,16 @@ while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        x -= speed
-    elif keys[pygame.K_RIGHT]:
-        x += speed
-    elif keys[pygame.K_UP]:
-        y -= speed
-    elif keys[pygame.K_DOWN]:
-        y += speed
-
-
     sc.fill(WHITE)
-    pygame.draw.rect(sc, BLUE, (x, y, 10, 20))
-    pygame.display.update()
+    for road_chunk in road_chunks:
+        if road_chunk[1][0] <= -2400:
+            road_chunk[1][0] = road_chunks[len(road_chunks) - 1][1][0] + 2400
 
+            road_chunks[0], road_chunks[1] = road_chunks[1], road_chunks[0]
+            break
+
+        road_chunk[1][0] -= speed
+        sc.blit(road_chunk[0], (road_chunk[1][0], road_chunk[1][1]))
+
+    pygame.display.update()
     clock.tick(FPS)
